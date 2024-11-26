de1423119ba4660ab0b468d581ac253f573ff169
Danka-Andras <danka.andras.balint@students.jedlik.eu>
2024-11-26 11:09:44 +0100

    Solution kezdetek

--- src/Solution.ts
+++ src/Solution.ts
@@ -1,5 +1,18 @@
 import fs from "fs";
+import Utca from "./Utca";
 
-export default class Solution{
+export default class Solution {
+    #utca: Utca[] = [];
 
+    constructor(forrás: string) {
+        fs.readFileSync(forrás)
+            .toString()
+            .split("\n")
+            .forEach(sor => {
+                const aktSor: string = sor.trim();
+                if (aktSor.length > 0) {
+                    this.#utca.push(new Utca(aktSor));
+                }
+            });
+    }
 }
